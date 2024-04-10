b719a12e1e83964e541c2afbea42d099cf4df863
Nikhil Raj <nikhilraaz045@gmail.com>
2024-04-10 21:16:45 +0530

    Initial commit

---
+++ javascript/signupReasons.js
@@ -0,0 +1,32 @@
+const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+const option = document.querySelectorAll(".option");
+const optionPara = document.querySelectorAll(".option-para");
+const finishButton = document.querySelector(".navigate-to-finish");
+const multiSelectInfo = document.querySelector(".multi-select-info");
+
+// Function to check if any checkbox is selected
+function isAnyCheckboxChecked() {
+  return [...checkboxes].some((checkbox) => checkbox.checked);
+}
+
+checkboxes.forEach((checkbox, index) => {
+  checkbox.addEventListener("change", () => {
+    option[index].classList.toggle("checked");
+    optionPara[index].classList.toggle("show");
+    finishButton.disabled = !isAnyCheckboxChecked();
+
+    updateMultiSelectInfo();
+  });
+});
+
+function updateMultiSelectInfo() {
+  multiSelectInfo.style.display = finishButton.disabled ? "none" : "block";
+}
+
+updateMultiSelectInfo();
+finishButton.disabled = !isAnyCheckboxChecked();
+
+finishButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  window.location.href = "emailVerification.html";
+});
